3e20cc31858daebd77ba0fd72f7e25a852fb4fa5
Ramsses <rpalafox@uabc.edu.mx>
2019-08-16 19:34:50 -0700

    Practica 0 - Introduccion a Unity
    Materiales, Physics, Texturas y Scripts

---
+++ Practica 0/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Its time to bounce some balls");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		
+		Debug.Log("Colison detectada con" + collision.gameObject.name);	
+		
+	}
+}

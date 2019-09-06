329df7133a9750fe3e6c10647adf8a8603fcf00f
Ramsses <rxtechtips@gmail.com>
2019-09-05 18:22:31 -0700

    Practica 1 - 2D Platformer v1.1
    Ajustes de diseño y re-estructuración de escenario

---
+++ Practica 1 - 2D Platformer/Assets/Scroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+	
+	public GameObject platillo;
+	int index = 0;
+	public int MAX_X;
+	float dir = 0.3f;
+	float last_pos_x;
+	float new_pos_x;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		movement();
+	}
+	
+	public void movement()
+	{
+		index++;
+		
+		last_pos_x = platillo.transform.position.x;
+		new_pos_x = last_pos_x+dir;
+		platillo.transform.position = new Vector3(new_pos_x,platillo.transform.position.y,platillo.transform.position.z);
+		if(index==MAX_X)
+		{
+			index = 0;
+			if(dir == 0.3f)
+				dir = -0.3f;
+			else
+				dir = 0.3f;
+		}
+		
+		
+		
+	}
+}
